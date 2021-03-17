af2e7ab5301fe02930d21502530c34c85b92ae7c
Insane <valeev.timur.v@gmail.com>
2021-03-17 09:23:08 +0300

    Add Dockerfile redis Add Queue Add ShortLink

---
+++ src/main/java/com/example/shortcutservice/common/LinksConfiguration.java
@@ -0,0 +1,34 @@
+package com.example.shortcutservice.common;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author
+ * @return
+ */
+
+@Configuration
+public class LinksConfiguration {
+
+    @Bean
+    Queue<String> linksQueue() {
+        Queue<String> res = new LinkedList<>();
+        String base = "abcdefghijklmnopqrstuvwxyz1234567890";
+        for (int a = 0; a < base.length(); a++) {
+            for (int b = 0; b < base.length(); b++) {
+                for (int c = 0; c < base.length(); c++) {
+                    res.add(String.valueOf(base.charAt(a)) +
+                            base.charAt(b) +
+                            base.charAt(c)
+                    );
+                }
+            }
+        }
+        return res;
+    }
+}
