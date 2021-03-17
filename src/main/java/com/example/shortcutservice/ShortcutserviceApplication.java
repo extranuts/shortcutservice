238d5732bd02bafa0dbd8ced22944ff1d712a2ce
Insane <valeev.timur.v@gmail.com>
2021-03-17 09:05:53 +0300

    Initial commit

---
+++ src/main/java/com/example/shortcutservice/ShortcutserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.shortcutservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortcutserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShortcutserviceApplication.class, args);
+    }
+
+}
